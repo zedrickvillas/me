68d2c7a231f7c6012b68d27d9c7e1ddbb1877196
Zedrick John Villas <zedrickvillas@gmail.com>
2018-06-21 13:57:58 +0800

    Add Transitions

--- src/js/app.js
+++ src/js/app.js
@@ -117,6 +117,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	  },
 	  "retina_detect": true
   });
+
+  setTimeout(function(){
+    $('.particles-js-canvas-el').toggleClass('full-opacity');
+    setTimeout(function(){
+      $('section').toggleClass('full-opacity');
+    },1000);
+  },1000);
 }, false);
 
 function rgb2hex(rgb) {
