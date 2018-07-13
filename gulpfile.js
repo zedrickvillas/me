5bdcae7bffa77aa17884a445c0722eb2c70f33d5
Zedrick John Villas <zedrickvillas@gmail.com>
2018-07-13 15:12:09 +0800

    - Correct Gulp  - Add Contact Section  - Add Contact Design

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['js','jekyll-rebuild']);
   gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
   gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-build'],['jekyll-rebuild']);
 });
 
 gulp.task('default', ['js', 'sass','imagemin', 'fonts', 'browser-sync', 'watch']);
